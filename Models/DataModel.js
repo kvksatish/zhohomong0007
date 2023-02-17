8c7a22222fb22ef0a9d05c56d61745e05f4ab671
kvksatish <kvksatish98@gmail.com>
2023-02-17 10:13:29 +0530

    newwww

--- Models/DataModel.js
+++ Models/DataModel.js
@@ -1,9 +1,12 @@
-// const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-// const datascheme = new mongoose.Schema({
-//     data: Array
-// })
-// const DataModel = mongoose.model("data", datascheme)
+// Set strictQuery to false to prepare for Mongoose 7 deprecation
+mongoose.set("strictQuery", false);
 
+const datascheme = new mongoose.Schema({
+    data: Array
+});
 
-// module.exports = { DataModel }
+const DataModel = mongoose.model("data", datascheme);
+
+module.exports = { DataModel };
