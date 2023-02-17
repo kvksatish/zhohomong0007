b54e0bd22a1ed90628db01f6bb7319b6568347c8
kvksatish <kvksatish98@gmail.com>
2023-02-17 10:07:01 +0530

    newwww

--- Models/DataModel.js
+++ Models/DataModel.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose")
+// const mongoose = require("mongoose")
 
-const datascheme = new mongoose.Schema({
-    data: Array
-})
-const DataModel = mongoose.model("data", datascheme)
+// const datascheme = new mongoose.Schema({
+//     data: Array
+// })
+// const DataModel = mongoose.model("data", datascheme)
 
 
-module.exports = { DataModel }
+// module.exports = { DataModel }
