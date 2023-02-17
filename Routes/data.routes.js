98fdf0201c61783a5e9026d1fd07fb492453e328
kvksatish <kvksatish98@gmail.com>
2023-02-17 11:51:48 +0530

    ne77w

--- Routes/data.routes.js
+++ Routes/data.routes.js
@@ -31,10 +31,10 @@ async function alldata() {
 }
 
 
-// async function bulkapi(accessToken, nextGet) {
-//     let data = await axios.get(`https://zohobulkapi.vercel.app/getdata?accessToken=${accessToken}&nextGet=${nextGet}`);
-//     return data;
-// }
+async function bulkapi(accessToken, nextGet) {
+    let data = await axios.get(`https://zohobulkapi.vercel.app/getdata?accessToken=${accessToken}&nextGet=${nextGet}`);
+    return data;
+}
 
 // //////////////////functions//////////////////////
 
@@ -50,43 +50,45 @@ async function alldata() {
 // //////////////////////timers////////////////////////
 
 
-let requestin30min = false
-let timeoutId;
+// let requestin30min = false
+// let timeoutId;
 
-function debouncedFunction() {
-    requestin30min = true
-    if (timeoutId) {
-        clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-        requestin30min = false
-        timeoutId = null;
-    }, 1000 * 60 * 10);
-}
+// function debouncedFunction() {
+//     requestin30min = true
+//     if (timeoutId) {
+//         clearTimeout(timeoutId);
+//     }
+//     timeoutId = setTimeout(() => {
+//         requestin30min = false
+//         timeoutId = null;
+//     }, 1000 * 60 * 10);
+// }
 
 
 
-setInterval(() => {
-    if (requestin30min) {
-        alldata().then((res) => {
-            console.log("alldata exe")
-            // writeData(res).then((res) => {
+// setInterval(() => {
+//     if (requestin30min) {
+//         alldata().then((res) => {
+//             console.log("alldata exe")
+//             // writeData(res).then((res) => {
 
-            // }).catch((err) => {
-            // })
-        })
-    }
-}, 1000 * 60 * 1);
+//             // }).catch((err) => {
+//             // })
+//         })
+//     }
+// }, 1000 * 60 * 1);
 
 
 //////////////////writing/////////////////////////
 
 dataController.get("/get", async (req, res) => {
 
-    debouncedFunction()
-
+    // debouncedFunction()
     const result = await DataModel.find()
     res.send(result)
+    alldata().then((res) => {
+        console.log(res)
+    })
 })
 
 
